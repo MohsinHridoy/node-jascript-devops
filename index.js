013be47aff68b16f4140a5d1d828b00968b620b3
USER <mohsingold9393@gmail.com>
2023-05-18 04:27:03 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
 app.use(express.static('public/'));
-app.listen(8080), function(){
+app.listen(3000), function(){
     console.log('server listening at 8080');
 }
